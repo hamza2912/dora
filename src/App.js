df3aeed7754d3dcab8c1307bfc082e4e4b4e89ec
Hamza Ali <hamzaali2912@gmail.com>
2022-03-30 17:00:28 +0500

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import './App.css';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./pages/home";
-// import Home2 from './pages/home2';
-import About from "./pages/about";
-import Shop from "./pages/shop";
-import Digital from './pages/digital';
 
 function App() {
   return (
@@ -12,9 +8,6 @@ function App() {
 
     <Switch>
 
-      <Route path="/digital" component={Digital} />
-      <Route path="/shop" component={Shop} />
-      <Route path="/about" component={About} />
       <Route path="/" component={Home} />
 
     </Switch>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import Header from '../components/header';
-import Artwork_modals from '../components/artwork_modals';
-import Footer from '../components/footer';
-import Available_artworks from '../components/available_artworks';
 
 function Home() {
 
@@ -10,50 +6,8 @@ function Home() {
       
       <div>
 
-        <Artwork_modals />
 
-        <Header />
-
-        <div className='main_body bg-home'>
-          {/* landing page */}
-          <div className='w-full lg:h-auto lg:pt-32 pt-32 lg:px-16 px-5 landing-page'>
-            {/* <div className='w-full flex lg:flex-row flex-col h-full'>
-              <div className='lg:w-1/2 w-full hidden'>
-
-              </div>
-              <div className='lg:w-full w-full h-full flex justify-center items-center mt-2 lg:mt-0'>
-                <p className='lg:text-3xl text-xl text-white font-courier lg:max-w-2xl text-justify'>Moments in time met with computer error, glitched into an entirely new phenomenon — all tangible via NFT. Archive91 displays the raw truth of the dot com bubble collapse while reminding us of our human tendency to repeat the past.</p>
-              </div>
-            </div> */}
-          </div>
-          {/* our collection */}
-          <div className='w-full lg:py-8 lg:px-32 px-5 py-5 mt-6'>
-            <Available_artworks />
-          </div>
-          {/* read more */}
-          <div className='w-full py-16 read-page'>
-            <h2 className='text-right text-5xl text-black font-presto italic text-white text-shadow-lg lg:mr-32 mr-10'>More on Artist...</h2>
-            <div className='grid lg:grid-cols-3 grid-cols-1 gap-4 p-6 border-2 border-white my-12'>
-              <div className='w-full lg:h-96 h-56 bg-gray border-2 border-black flex justify-center items-center cursor-pointer'>
-                <a className='w-4/5' href="https://www.nytimes.com/2001/07/30/nyregion/a-debut-in-queens-nighthawks-at-the-museum.html?searchResultPosition=7" rel="noreferrer" target="_blank">
-                  <img src="images/logo/new_york_times_logo.png" alt="New York Times" />
-                </a>              
-              </div>
-              <div className='w-full lg:h-96 h-56 bg-gray border-2 border-black flex justify-center items-center cursor-pointer'>
-                <a className='w-4/5' href="https://www.startribune.com/artist-matthew-bakkom-marks-minneapolis-homecoming-with-diy-exhibit-of-wwi-photography/500151282/" rel="noreferrer" target="_blank">
-                  <img src="images/logo/star_tribune_logo.png" alt="New York Times" />
-                </a>
-              </div>
-              <div className='w-full lg:h-96 h-56 bg-gray border-2 border-black flex justify-center items-center cursor-pointer'>
-                <a className='w-1/4' href="https://madparis.fr/francais/bibliotheque/expositions/expositions-terminees/presentation-2904" rel="noreferrer" target="_blank">
-                  <img src="images/logo/mad_paris_logo.png" alt="New York Times" />
-                </a>
-              </div>
-            </div>
-          </div>
-          {/* footer */}
-          <Footer></Footer>
-        </div>
+        
 
       </div>
 
